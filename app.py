514eb809a4d60ec4992033f98b5732a155433a1a
smp <smpenna3@gmail.com>
2018-06-20 22:42:03 -0400

    Added scheduling and RPi GPIO configuration

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
 from flask import Flask, render_template, request
-import RPi.GPIO as gpio
+try:
+	import RPi.GPIO as gpio
+except:
+	print('RPi.GPIO not found')
+from apscheduler.schedulers.background import BackgroundScheduler
+import logging
 
+# Setup logging
+logger = logging.getLogger('mainLog')
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('log.log')
+fh.setLevel(logging.DEBUG)
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(lineno)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+sh.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(sh)
+
+# Setup scheduler
+s = BackgroundScheduler(misfire_grace_time=60, max_instances=1, timezone='America/New_York')
+s.start()
+logger.info('Scheduler setup')
 
 # Pins for relay lines
 relay1 = 0
@@ -9,24 +31,59 @@ relay3 = 2
 relay4 = 3
 
 # Setup GPIO lines
-gpio.setmode(gpio.BCM)
-gpio.setwarnings(False)
+try:
+	gpio.setmode(gpio.BCM)
+	gpio.setwarnings(False)
+except:
+	logger.error('Could not setup GPIO')
 
 app = Flask(__name__)
 
+# Define function to turn off the fan gpio line
+def turnOff():
+	logger.info('Turning off')
+	# Try to remove the turn off job
+	try:
+		s.remove_job('turnoff')
+		logger.debug('Removed scheduler job')
+	except:
+		logger.debug("Didn't find a scheduler job")
+		pass
+
+# Define function to turn on fan gpio line
+def turnOn():
+	logger.info("Turning on")
+
 @app.route('/', methods=['POST', 'GET'])
 def home():
+	s.print_jobs()
 	if request.method == 'POST':
 		if 'on' in request.form:
-			print('on')	
+			turnOn()
+
 		elif 'off' in request.form:
-			print('off')
+			turnOff()
+
 		elif 'thirty' in request.form:
-			print('30 minutes')
+			turnOn()
+
+			# Setup timer
+			s.add_job(turnOff, 'interval', minutes=30, id='turnoff')
+			logger.info('Set timer for 30 minutes')
+
 		elif 'hour' in request.form:
-			print('one hour')
+			turnOn()
+
+			# Setup timer
+			s.add_job(turnOff, 'interval', hours=1, id='turnoff')
+			logger.info('Set timer for 1 hour')
+
 		elif 'twohour' in request.form:
-			print('two hours')
+			turnOn()
+
+			# Setup timer
+			s.add_job(turnOff, 'interval', hours=2, id='turnoff')
+			logger.info('Set timer for 2 hours')
 
 	return render_template('home.html')
 
